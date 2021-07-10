0abeb26e648b1926272bba6bebd840d10422499b
unknown <isaacferrazlima77@gmail.com>
2021-07-10 13:38:19 -0300

    Use process env instead of config json

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 
 const Discord = require('discord.js')
 const client = new Discord.Client()
-const config = require('./config.json')
+// const config = require('./config.json')
 
 client.on('ready', () => {
   console.log('O bot está pronto.')
 })
 
-client.login(config.token)
+client.login(process.env.DJS_TOKEN)
